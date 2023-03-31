0d5ced54aa530f0bef24480c9ed745413e3480c4
AvaOleh <avakimyanov@gmail.com>
2023-03-31 18:02:31 +0300

    add tests for operating with files (csv, pdf, xlsx, zip)

--- conftest.py
+++ conftest.py
@@ -1,10 +1,21 @@
 import pytest
+import glob
 import os
 from selene.support.shared import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
 from utils import attach
+
+path_output_files = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'resources')
+
+
+@pytest.fixture()
+def clear_dir():
+    """Fixture: delete all files from dir with archive"""
+    all_files = os.path.join(path_output_files, '*.*')
+    for file in glob.glob(all_files):
+        os.remove(file)
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -14,9 +25,6 @@ def load_env():
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
-    browser.config.base_url = "https://demoqa.com"
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
 
     options = Options()
     selenoid_capabilities = {
@@ -37,9 +45,12 @@ def open_browser():
         options=options
     )
     browser.config.driver = driver
+    browser.config.base_url = os.getenv('DEMO_QA_BASE_URL')
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
 
+    yield browser
 
-    yield
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
